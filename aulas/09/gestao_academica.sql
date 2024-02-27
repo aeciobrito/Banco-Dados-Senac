201e1b27973857beda92017b036a073051c4827e
aeciobrito <aeciobrito@gmail.com>
2024-02-26 22:00:28 -0300

    09 - Finalizacao script para DQL

--- aulas/09/gestao_academica.sql
+++ aulas/09/gestao_academica.sql
@@ -58,3 +58,56 @@ CREATE TABLE IF NOT EXISTS professores_disciplinas(
     FOREIGN KEY (Professor_ID) REFERENCES Professor(Id),
     FOREIGN KEY (Disciplina_ID) REFERENCES Disciplina(Id)
 );
+
+-- -----------------------------------
+-- DML - Inserção de Dados nas Tabelas
+-- -----------------------------------
+INSERT INTO Aluno (Nome, Cpf) VALUES 
+	('Carlos Silva', '12345678901'),
+    ('Pedro Rocha', '23456789012'),
+    ('Monica Souza', '34567890123'),
+    ('Fabio Silva', '45678901234');
+
+INSERT INTO Curso (Nome) VALUES
+	('Ciência da Computação'),
+    ('Engenharia Civil'),
+    ('Administração'),
+    ('Medicina'),
+    ('Arquitetura');
+
+INSERT INTO Matricula(Aluno_ID, Curso_ID, DataMatricula) VALUES
+	(1, 2, '2024-02-01'),
+    (1, 5, '2024-02-02'),
+    (2, 2, '2024-01-02'),
+    (2, 5, '2024-02-01'),
+    (3, 2, '2024-01-15'),
+    (3, 5, '2024-01-29');
+    
+INSERT INTO Professor (Nome, Especializacao) VALUES
+	('Huguinho', 'Tecnologia'),
+    ('Zezinho', 'Idiomas'),
+    ('Luizinho', 'Cálculo'),
+    ('Tico', 'Tecnoloia'),
+    ('Teco', 'Idiomas');
+
+INSERT INTO Disciplina (Nome, Horas) VALUES
+	('Matemática "Avançada" II', 40),
+    ('Inglês Técnico', 50),
+    ('Linguagem de Programação', 45);
+
+INSERT INTO Professores_Disciplinas (Professor_ID, Disciplina_ID) VALUES
+	(1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 1),
+    (5, 2);
+
+INSERT INTO Disciplinas_Cursos(Disciplina_ID, Curso_ID) VALUES
+	(1, 1),
+    (2, 2),
+    (3, 3);
+
+
+
+
+
