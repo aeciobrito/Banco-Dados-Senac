1d0eb312a0611e6a7376a1e42de42528beb5149e
aeciobrito <aeciobrito@gmail.com>
2024-03-08 20:35:04 -0300

    Resolucao atividade aula 17 - parte 2

--- aulas/17/atividade_consultas_hotel.sql
+++ aulas/17/atividade_consultas_hotel.sql
@@ -18,8 +18,31 @@ SELECT * FROM Item WHERE descricaoItem LIKE ('%de%') LIMIT 100 OFFSET 2;
 
 # Consultas com JOINS
 -- Selecioar detalhes da hospedagem, incluindo informações do cliente
+SELECT Hospedagem.*, Cliente.nomeCliente
+FROM Hospedagem
+JOIN Cliente ON Hospedagem.codCliente = Cliente.codCliente;
+
 -- Selecionar chalés com os itens associados
+SELECT Chale.*, Item.nomeItem AS Itens
+FROM Chale
+LEFT JOIN chale_item ON Chale.codChale = chale_item.codChale
+LEFT JOIN item ON chale_item.nomeItem = item.nomeItem;
+
 -- Selecionar serviços utilizados em uma hospedagem
+SELECT Servico.*, hospedagem_servico.dataServico
+FROM hospedagem_servico
+INNER JOIN servico ON  hospedagem_servico.codServico = servico.codServico
+WHERE hospedagem_servico.codHospedagem = 1;
+
 -- Consultar os clientes com seus telefones
+SELECT Cliente.*, GROUP_CONCAT(telefone.telefone) AS Telefone
+FROM Cliente
+LEFT JOIN Telefone ON Cliente.codCliente = Telefone.codCliente
+GROUP BY Cliente.codCliente;
+
 -- Selecionar chalés ocupados em uma data específica
+SELECT Chale.*, hospedagem.dataInicio, hospedagem.dataFim
+FROM chale
+JOIN Hospedagem ON chale.codChale = hospedagem.codChale
+WHERE '2024-03-25' BETWEEN hospedagem.dataInicio AND hospedagem.dataFim;
 
