3efe078f7e7bca0243e654ebcd3684f66f35ee1c
aeciobrito <aeciobrito@gmail.com>
2024-03-07 21:13:08 -0300

    17 - Consultas no BD de hotel

---
+++ aulas/17/atividade_consultas_hotel.sql
@@ -0,0 +1,14 @@
+## Utilize o banco de dados hotel (disponível no github) para responder a atividade
+# Consultas sem JOINS
+-- Consulte todos os clientes de São Paulo (SP)
+-- Selecionar hospedagens com mais de 3 pessoas e desconto igual a 0
+-- Selecionar telefones de clientes com ID 1 ou 2, limitando a 2 resultados
+-- Selecionar chalés com capacidades entre 4 e 6, ordenaos por valor em alta estação
+-- Selecione todos os itens que contenham "de" na descrição, pulando os 2 primeiros resultados
+
+# Consultas com JOINS
+-- Selecioar detalhes da hospedagem, incluindo informações do cliente
+-- Selecionar chalés com os itens associados
+-- Selecionar serviços utilizados em uma hospedagem
+-- Consultar os clientes com seus telefones
+-- Selecionar chalés ocupados em uma data específica
